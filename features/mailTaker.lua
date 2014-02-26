d4ab80e478d163f15a23653a4ff8c803184e3d98
andy <andy@andydote.co.uk>
2014-02-26 22:42:21 +0000

    features.mailTaker: stub out feature take gold and items from mail

---
+++ features/mailTaker.lua
@@ -0,0 +1,113 @@
+local addon, ns = ...
+local config = ns.config
+
+local core = Dark.core
+local events = core.events.new()
+
+local mailProcessor = {
+
+	new = function(action)
+
+		local this = {}
+
+		local printMessage = function(message)
+			print("Dark.Minion: Mail:", message)
+		end
+
+		local onErrorMessage = function(self, event, message)
+			if message == ERR_INV_FULL then
+				finally()
+				printMessage("Stopped, Inventory was full.")
+			end
+		end
+
+		local finally = function()
+			events.unregister("UI_ERROR_MESSAGE")
+			events.unregisterOnUpdate()
+		end
+
+		local onUpdate = function()
+
+			if not InboxFrame:IsVisible() then
+				finally()
+				printMessage("No mailbox open/in range.")
+			end
+
+		end
+
+
+
+		this.process = function()
+			events.register("UI_ERROR_MESSAGE", onErrorMessage)
+			events.registerOnUpdate(onUpdate)
+		end
+
+		return this
+
+	end,
+
+}
+
+
+
+local takeAll = function()
+
+	local action = function(index)
+		AutoLootMailItem(index)
+	end
+
+	local mailbox = mailProcessor.new(action)
+	mailbox.process()
+
+end
+
+local takeGold = function()
+
+	local action = function(index)
+		TakeInboxMoney(index)
+	end
+
+	local mailbox = mailProcessor.new(action)
+	mailbox.process()
+
+end
+
+local createUi = function()
+
+	local itemsButton = CreateFrame("Button", "DarkMinionTakeAll", InboxFrame, "UIPanelButtonTemplate")
+	itemsButton:SetSize(60, 25)
+	itemsButton:SetText("Take All")
+	itemsButton:SetScript("OnClick", takeAll)
+
+	local goldButton = CreateFrame("Button", "DarkMinionTakeGold", InboxFrame, "UIPanelButtonTemplate")
+	goldButton:SetSize(60, 25)
+	goldButton:SetText("Take Gold")
+	goldButton:SetScript("OnClick", takeGold)
+
+	itemsButton:SetPoint("TOPLEFT", InboxFrame, "TOPLEFT", 80, -40)
+	goldButton:SetPoint("LEFT", itemsButton, "RIGHT", 10, 0)
+
+end
+
+
+ns.features.add({
+
+	name = "Mail Taker",
+
+	initialise = function()
+		createUi()
+	end,
+
+	enable = function()
+
+	end,
+
+	disable = function()
+
+	end,
+
+	isEnabled = function()
+
+	end,
+
+})
