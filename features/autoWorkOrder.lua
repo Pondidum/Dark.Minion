603f6ccd5a9c36af283f8c735899ff367df92fa5
andy <andy@andydote.co.uk>
2015-01-08 23:20:43 +0000

    autoWorkOrder: implement auto queuing

--- features/autoWorkOrder.lua
+++ features/autoWorkOrder.lua
@@ -4,15 +4,57 @@ local config = ns.config
 local core = Dark.core
 local events = core.events.new()
 
-local onOpen = function()
+local createUi = function()
 
-end
+	local parent = GarrisonCapacitiveDisplayFrame
+	local startButton = GarrisonCapacitiveDisplayFrame.StartWorkOrderButton
 
-local onClose = function()
+	local extraWidth = 40
+	local extraHeight = startButton:GetHeight()
 
-end
+	local extra = CreateFrame("Button", "DarkMinionAutoWorkOrder", parent, "MagicButtonTemplate")
+	extra:SetSize(extraWidth, extraHeight)
+	extra:SetPoint("BOTTOMRIGHT", parent, "BOTTOMRIGHT", -6, 4)
+	extra:SetText("All")
 
-local onCrafterInfo = function()
+	startButton:SetWidth(startButton:GetWidth() - extraWidth)
+	startButton:ClearAllPoints()
+	startButton:SetPoint("RIGHT", extra, "LEFT", 0, 0)
+
+	local throttle = 0.3
+	local total = 0
+	local maxShipments = 0
+
+	local onCrafterInfo = function(self, event, success, active, max, plotID)
+		maxShipments = max
+	end
+
+	local onDisplay = function()
+		startButton:SetScript("OnUpdate", nil)
+	end
+
+	events.register("SHIPMENT_CRAFTER_OPENED", onDisplay)
+	events.register("SHIPMENT_CRAFTER_CLOSED", onDisplay)
+	events.register("SHIPMENT_CRAFTER_INFO", onCrafterInfo)
+
+	local queueOrders = function(self, elapsed)
+		total = total + elapsed
+
+		if total > throttle then
+			total = 0
+
+			C_Garrison.RequestShipmentCreation()
+
+			 if C_Garrison.GetNumPendingShipments() == maxShipments then
+		        self:SetScript("OnUpdate", nil)
+     		end
+		end
+	end
+
+	startButton:SetScript("OnClick", function()
+		total = 0
+		startButton:SetScript("OnUpdate", queueOrders)
+	end)
 
 end
 
@@ -20,20 +62,17 @@ ns.features.add({
 
 	name = "AutoWorkOrder",
 
+	initialise = function()
+		createUi()
+	end,
+
 	enable = function()
-		events.register("SHIPMENT_CRAFTER_OPENED", onOpen)
-		events.register("SHIPMENT_CRAFTER_CLOSED", onClose)
-		events.register("SHIPMENT_CRAFTER_INFO", onCrafterInfo)
 	end,
 
 	disable = function()
-		events.unregister("SHIPMENT_CRAFTER_OPENED", onOpen)
-		events.unregister("SHIPMENT_CRAFTER_CLOSED", onClose)
-		events.unregister("SHIPMENT_CRAFTER_INFO", onCrafterInfo)
 	end,
 
 	isEnabled = function()
-		return events.isRegistered("SHIPMENT_CRAFTER_OPENED")
 	end,
 
 })
