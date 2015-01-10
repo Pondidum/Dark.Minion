ad5e973d0bba517a675c1fe632f146d44801d37c
andy <andy@andydote.co.uk>
2015-01-10 11:37:52 +0000

    autoRepair: refactor to use dark.class

--- features/autoRepair.lua
+++ features/autoRepair.lua
@@ -1,70 +1,60 @@
 local addon, ns = ...
-local config = ns.config
 
-local events = ns.lib.events.new()
-
-local IsShiftKeyDown = IsShiftKeyDown
-local CanMerchantRepair = CanMerchantRepair
-local GetRepairAllCost = GetRepairAllCost
-local CanGuildBankRepair = CanGuildBankRepair
-local RepairAllItems = RepairAllItems
-
-local autoRepair = function()
-
-	if IsShiftKeyDown() then
-		return
-	end
-
-	if not CanMerchantRepair() then
-		return
-	end
-
-	local cost, possible = GetRepairAllCost()
-	local useGuildBank = CanGuildBankRepair()
-
-
-	if cost > 0 then
-
-		if possible then
-
-			RepairAllItems(useGuildBank)
-
-			local copper = cost % 100
-			local silver = math.floor((cost % 10000) / 100)
-			local gold = math.floor(cost / 10000)
-
-			DEFAULT_CHAT_FRAME:AddMessage("Your gear has been repaired for |cffffffff"..gold.."g "..silver.."s "..copper.."c|r.", 255, 255, 0)
-
-		else
-
-			DEFAULT_CHAT_FRAME:AddMessage("You don't have enough money to repair!", 255, 0, 0)
-
-		end
-
-	end
-
-end
-
-local controller = {
-	enable = function()
-		events.register("MERCHANT_SHOW", autoRepair)
-	end,
-
-	disable = function()
-		events.unregister("MERCHANT_SHOW", autoRepair)
-	end,
-
-	isEnabled = function()
-		return events.isRegistered("MERCHANT_SHOW")
-	end,
-}
-
-ns.features.add({
+local autoRepair = Darker.class:extend({
 
 	name = "AutoRepair",
 
-	enable = controller.enable,
-	disable = controller.disable,
-	isEnabled = controller.isEnabled,
+	ctor = function(self)
+		self:include(Darker.events)
+	end,
 
+	enable = function(self)
+		self:register("MERCHANT_SHOW")
+	end,
+
+	disable = function(self)
+		self:unregister("MERCHANT_SHOW")
+	end,
+
+	isEnabled = function(self)
+		return self:isRegistered("MERCHANT_SHOW")
+	end,
+
+	MERCHANT_SHOW = function(self)
+
+		if IsShiftKeyDown() then
+			return
+		end
+
+		if not CanMerchantRepair() then
+			return
+		end
+
+		local cost, possible = GetRepairAllCost()
+		local useGuildBank = CanGuildBankRepair()
+
+
+		if cost > 0 then
+
+			if possible then
+
+				RepairAllItems(useGuildBank)
+
+				local copper = cost % 100
+				local silver = math.floor((cost % 10000) / 100)
+				local gold = math.floor(cost / 10000)
+
+				DEFAULT_CHAT_FRAME:AddMessage("Your gear has been repaired for |cffffffff"..gold.."g "..silver.."s "..copper.."c|r.", 255, 255, 0)
+
+			else
+
+				DEFAULT_CHAT_FRAME:AddMessage("You don't have enough money to repair!", 255, 0, 0)
+
+			end
+
+		end
+
+	end,
 })
+
+ns.features.add(autoRepair)
